4052f1341cff7001448e8d66330e6aa934241a01
BORJA TORREGROSA LORENTE <botolo@floridauniversitaria.es>
2020-10-27 08:47:31 +0100

    Ejercicio 18

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get install -y nginx
 rm -rf /etc/nginx/sites-enabled
-ln -s /vagrant/www ../usr/share/nginx/www/
+
 cp /vagrant/sites-enabled/default /etc/nginx/sites-enabled
 
 service nginx start
